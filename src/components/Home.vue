721867b2a19bb91c0f678e5823963dd98e79783d
joshuahoo <joshuahoo@foxmail.com>
2021-04-03 19:33:48 +0800

    完成登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+/* eslint-disable semi */
+<template>
+    <div>
+        <el-button type="info" @click="logout">退出</el-button>
+    </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    // 退出登录功能
+    logout () {
+    // 清空token
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
